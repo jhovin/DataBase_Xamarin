5f6aa087bbbb3679b3531adea3e441c8d16907b2
jhovin <jhovin.bonifacio@tecsup.edu.pe>
2020-06-22 17:39:00 -0500

    first commit

---
+++ XamEFCore/XamEFCore.iOS/Implementations/ConfigDataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using XamEFCore.Interfaces;
+using XamEFCore.iOS.Implementations;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace XamEFCore.iOS.Implementations
+{
+    class ConfigDataBase : IConfigDataBase
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseFileName);
+        }
+
+    }
+}
---
+++ XamEFCore/XamEFCore/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamEFCore.ViewModels;
+
+namespace XamEFCore.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new MainViewModel();
+        }
+    }
+}
